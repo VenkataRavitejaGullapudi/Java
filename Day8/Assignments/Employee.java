b96ddffc8fa3dd357486c3feaa9bb93b50c737b0
ravi777-developer <70102577+ravi777-developer@users.noreply.github.com>
2020-11-02 00:31:27 +0530

    Update Employee.java

--- Day8/Assignments/Employee.java
+++ Day8/Assignments/Employee.java
@@ -9,37 +9,27 @@ public class Employee {
     private String designation;
     Scanner sc = new Scanner(System.in);
 
-
-    // To only to see this age outside
-    public int getAge(){
-        return age;
-    }
-
-    //To edit or update the age
-    public void setAge(int age){
-        this.age=age;
-    }
-
     public void getDetails(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("--------------------- Enter the employee details -------------------");
-        System.out.print("Enter name of employee: ");
+        
+        System.out.print("Enter name: ");
         name = sc.nextLine();
-        System.out.print("Enter age of employee: ");
+        System.out.print("Enter age: ");
         age = sc.nextInt();
-        System.out.print("Enter salary of employee: ");
+        System.out.print("Enter salary: ");
         salary = sc.nextDouble();
-        System.out.print("Enter Designation of employee: ");
+        sc.nextLine();
+        System.out.print("Enter Designation: ");
         designation = sc.nextLine();
     }
 
     public void display()
     {
-        System.out.println("The Name of Employee is "+name);
-        System.out.print("The Age of Employee is ");
+        System.out.println("The Name is "+name);
+        System.out.print("The Age is ");
         System.out.println(age);
-        System.out.println("The salary of Employee is "+salary);
-        System.out.println("The Designation of Employee is "+designation);
+        System.out.println("The salary is "+salary);
+        System.out.println("The Designation is "+designation);
     }
 
 
